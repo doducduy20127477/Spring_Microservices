14c7936aa9d1b1e3268d601fa5398a96f4e71824
Đỗ Đức Duy <90464550+doducduy20127477@users.noreply.github.com>
2023-11-21 14:37:56 +0700

    feat(02): explore i18n

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,27 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController () {
+    }
+    @Autowired
+    public HelloWorldController (MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -17,5 +33,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World " + name);
+    }
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
