10f0233c0f9653765e0f339a1439a3cad55088b8
Đỗ Đức Duy <90464550+doducduy20127477@users.noreply.github.com>
2023-11-27 11:22:08 +0700

    feat: create a JPA Repository

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,10 +12,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
     @Autowired
+    private CurrencyExchangeRepository repository;
+    @Autowired
     private Environment environment;
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to Find data for: " + from + " to" + to);
+        }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
