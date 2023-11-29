c10fdea60d2681496644a3a5e0aa7ab6d4191d31
Đỗ Đức Duy <90464550+doducduy20127477@users.noreply.github.com>
2023-11-29 14:59:06 +0700

    feat: add fallback for CircuitBreaker

---
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-api", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
